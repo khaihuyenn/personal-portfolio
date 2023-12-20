57c10d610fbbba7e60bfff5746379a1512aec4af
khaihuyenn <khaihuyen.nguyen5211@gmail.com>
2023-12-20 17:41:16 -0500

    Modify code to fix the bug

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -26,33 +26,40 @@ export const NavBar = () => {
       setActiveLink(value);
     }
     return (
-          <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
-            <Container>
-              <Navbar.Brand href="home">
-                <img src={logo} alt="Logo" />
-              </Navbar.Brand>
-              <Navbar.Toggle aria-controls="basic-navbar-nav">
-                <span className="navbar-toggler-icon"></span>
-              </Navbar.Toggle>
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ms-auto">
-                  <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-                  <Nav.Link href="#education" className={activeLink === 'education' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('education')}>Education</Nav.Link>
-                  <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
-                  <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
-                </Nav>
-                <span className="navbar-text">
-                  <div className="social-icon">
-                    <a href="#"><img src={navIcon1} alt="" /></a>
-                    <a href="#"><img src={navIcon2} alt="" /></a>
-                    <a href="#"><img src={navIcon3} alt="" /></a>
-                  </div>
+      <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+      <Container>
+        <Navbar.Brand href="home">
+          <img src={logo} alt="Logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+            <Nav.Link href="#education" className={activeLink === 'education' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('education')}>Education</Nav.Link>
+            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+          </Nav>
+          <span className="navbar-text">
+            <div className="social-icon">
+              <button onClick={() => console.log('Social Icon 1')}>
+                <img src={navIcon1} alt="" />
+              </button>
+              <button onClick={() => console.log('Social Icon 2')}>
+                <img src={navIcon2} alt="" />
+              </button>
+              <button onClick={() => console.log('Social Icon 3')}>
+                <img src={navIcon3} alt="" />
+              </button>
+            </div>
 
-                  <button className="vvd"onClick={() => console.log('connect')}><span>Let's Connect</span></button>
-                
-                </span>
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
+            <button className="vvd" onClick={() => console.log('connect')}>
+              <span>Let's Connect</span>
+            </button>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
       )
 }
