41553eb4ace9326cd348dd4852b77f66a4fb7f92
khaihuyenn <khaihuyen.nguyen5211@gmail.com>
2023-12-20 19:58:45 -0500

    Change second social icon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -40,17 +40,24 @@ export const NavBar = () => {
             <Nav.Link href="#education" className={activeLink === 'education' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('education')}>Education</Nav.Link>
             <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
             <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
-          </Nav>
-          <span className="navbar-text">
-              <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
-              </div>
-              <HashLink to='#connect'>
-                <button className="vvd"><span>Letss Connect</span></button>
-              </HashLink>
-            </span>
+            </Nav>
+            <span className="navbar-text">
+             <div className="social-icon">
+               <button onClick={() => console.log('Social Icon 1')}>
+                 <img src={navIcon1} alt="" />
+               </button>
+               <button onClick={() => console.log('Social Icon 2')}>
+                 <img src={navIcon2} alt="" />
+               </button>
+               <button onClick={() => console.log('Social Icon 3')}>
+                 <img src={navIcon3} alt="" />
+               </button>
+             </div>
+
+             <button className="vvd" onClick={() => console.log('connect')}>
+              <span>Let's Connect</span>
+             </button>
+           </span>
         </Navbar.Collapse>
       </Container>
     </Navbar>
