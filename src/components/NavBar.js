e0ca57bc4b04984ba9ebde233934abbeed82e2ba
khaihuyenn <khaihuyen.nguyen5211@gmail.com>
2024-05-03 20:05:42 -0400

    Navigation to social pages

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -47,9 +47,9 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
+                <a href="https://www.linkedin.com/in/khaihuyennn-1a4a72190" target="_blank"><img src={navIcon1} alt="LinkedIn" /></a>
+                <a href="https://github.com/khaihuyenn" target="_blank"><img src={navIcon2} alt="GitHub" /></a>
+                <a href="https://www.instagram.com/khnguyeenx/" target="_blank"><img src={navIcon3} alt="Instagram" /></a>
               </div>
               <HashLink to='#connect'>
                 <button className="vvd"><span>Let’s Connect</span></button>
